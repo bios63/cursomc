2352a03d9cfb1364a7f8c86640f43e26eea526ec
Bios63 <bios63@outlook.com>
2017-11-20 23:09:41 -0200

    Categoria

--- src/main/java/com/cavaco/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cavaco/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,30 @@
 package com.cavaco.cursomc.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cavaco.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest funcionando";
+		Categoria cat1 = new Categoria(1,"Curso 1");
+		Categoria cat2 = new Categoria(2,"Cusrso 2");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
